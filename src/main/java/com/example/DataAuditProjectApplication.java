363ff8750eaf894f197fff8781e033ded43e6827
sunnapudisatish96-sketch <sunnapudisatish96@gmail.com>
2025-12-04 07:17:48 +0530

    Add files via upload

---
+++ src/main/java/com/example/DataAuditProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAuditProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAuditProjectApplication.class, args);
+	}
+
+}
