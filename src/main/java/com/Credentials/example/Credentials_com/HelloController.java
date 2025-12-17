7f34d49ae178dadf337f6f73294e3b639284ba08
satish sunnapudi <sunnapudisatish96@gmail.com>
2025-12-17 17:03:58 +0530

    Intial commit

---
+++ src/main/java/com/Credentials/example/Credentials_com/HelloController.java
@@ -0,0 +1,13 @@
+package com.Credentials.example.Credentials_com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "welcome you logged in successfully";
+	}
+
+}
---
+++ src/test/java/com/Credentials/example/Credentials_com/CredentialsComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Credentials.example.Credentials_com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredentialsComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
