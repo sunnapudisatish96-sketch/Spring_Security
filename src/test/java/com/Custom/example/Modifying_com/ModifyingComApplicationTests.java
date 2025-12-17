8430ee6fef03fd74ec8d20f813d9dfd128118d83
satish sunnapudi <sunnapudisatish96@gmail.com>
2025-12-17 18:32:41 +0530

    added files

---
+++ src/test/java/com/Custom/example/Modifying_com/ModifyingComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Custom.example.Modifying_com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModifyingComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
